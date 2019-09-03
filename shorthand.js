c09e01628076ddc7cf9c28c0f2453f43a54acdc7
LimboweSource <43504844+LimboweSource@users.noreply.github.com>
2019-09-03 23:51:46 +0100

    Add files via upload

---
+++ shorthand.js
@@ -0,0 +1,6 @@
+const log = console.log.bind();
+const err = console.error.bind();
+const get = document.querySelector.bind(document);
+const getAll = document.querySelectorAll.bind(document);
+const getById = document.getElementById.bind(document);
+const getByClass = document.getElementsByClassName.bind(document);
